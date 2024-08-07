6b0a8a71c05f6814ab67338fef12863d401ba17d
Yujin1219 <127809173+Yujin1219@users.noreply.github.com>
2024-08-07 18:26:57 +0900

    [level 3] Title: 가장 먼 노드, Time: 37.17 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,8 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
+        
         for(int i = 1; i <= n; i++)
-            graph.put(i, new ArrayList<>()); // graph를 저장할 맵을 초기화
+            graph.put(i, new ArrayList<>());
         
         for(int[] e : edge){
             graph.get(e[0]).add(e[1]);
@@ -13,8 +14,8 @@ class Solution {
         
         boolean[] visited = new boolean[n + 1];
         Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{1, 0}); // 시작 노드 1, 초기 거리 0
-        visited[1] = true; // 시작 노드 방문 처리
+        q.add(new int[]{1, 0});
+        visited[1] = true;
         
         int maxDist = 0, count = 0;
         while(!q.isEmpty()){
@@ -22,14 +23,16 @@ class Solution {
             
             if(maxDist < cur[1]){
                 maxDist = cur[1];
-                count = 1; // 초기화
-            }else if(maxDist == cur[1]) count++;
+                count = 1;
+            }else if(maxDist == cur[1])
+                count++;
             
             for(int next : graph.get(cur[0])){
-                if(!visited[next]){
+                if(visited[next]) continue;
+                else{
                     visited[next] = true;
                     q.add(new int[] { next, cur[1] + 1});
-                }else continue;
+                }
             }
         }
         return count;
