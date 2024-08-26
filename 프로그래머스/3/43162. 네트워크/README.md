8d2be1134f8d8847742959a2111242b8dd58fa1b
Yujin1219 <127809173+Yujin1219@users.noreply.github.com>
2024-08-26 17:12:50 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 0.27 ms
+메모리: 77.1 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 05일 22:08:42
+2024년 08월 26일 17:12:47
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,23 +1,24 @@
 class Solution {
+    boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n];
+        visited = new boolean[n];
         
         for(int i = 0; i < n; i++){
             if(!visited[i]){
-                dfs(computers, visited, i);
+                dfs(computers, i);
                 answer++;
             }
         }
         return answer;
     }
     
-    public void dfs(int[][] computers, boolean[] visited, int i){
+    void dfs(int[][] computers, int i){
         visited[i] = true;
         
         for(int j = 0; j < computers.length; j++){
             if(!visited[j] && computers[i][j] == 1)
-                dfs(computers, visited, j);
+                dfs(computers, j);
         }
     }
 }
