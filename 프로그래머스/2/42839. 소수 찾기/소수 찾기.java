0773a7ff4598c1b29839b7af665c987d68c54af3
Yujin1219 <127809173+Yujin1219@users.noreply.github.com>
2024-08-26 14:36:16 +0900

    [level 2] Title: 소수 찾기, Time: 4.67 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,45 +1,6 @@
-import java.util.*;
-
 class Solution {
-    
-    Set<Integer> check = new HashSet<>();
-    boolean[] visited;
     public int solution(String numbers) {
-        visited = new boolean[numbers.length()];
-        p(numbers, 0, 0);
-        
         int answer = 0;
-        for(int num : check)
-            if(isPrime(num)) answer++;
-            
-            return answer;
-        
-        
-    }
-    
-    void p(String numbers, int cur, int digit){
-        if(digit == numbers.length()) return;
-        
-        for(int i = 0; i < numbers.length(); i++){
-            if(visited[i]) continue;
-            
-            int newValue = cur + (int)((numbers.charAt(i) - '0') * Math.pow(10, digit));
-            
-            check.add(newValue);
-            
-            visited[i] = true;
-            p(numbers, newValue, digit + 1);
-            
-            visited[i] = false;
-        }
-    }
-    
-    boolean isPrime(int n){
-        if(n < 2) return false;
-        
-        for(int i = 2; i * i <= n; i++)
-            if(n % i == 0) return false;
-        
-        return true;
+        return answer;
     }
 }
